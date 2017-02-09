18db60111a28c6027076b0f5bcab75a3ad3e74ed
MyMilkedEek <eek@mymilkedeek.net>
2017-02-09 22:44:46 +0100

    initial push, setting up structure and running code

---
+++ core/src/net/mymilkedeek/ecsplayground/GameInstance.java
@@ -0,0 +1,11 @@
+package net.mymilkedeek.ecsplayground;
+
+import com.badlogic.gdx.Game;
+
+public class GameInstance extends Game {
+
+	@Override
+	public void create() {
+
+	}
+}
---
+++ desktop/src/net/mymilkedeek/ecsplayground/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.mymilkedeek.ecsplayground.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.mymilkedeek.ecsplayground.GameInstance;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameInstance(), config);
+	}
+}
