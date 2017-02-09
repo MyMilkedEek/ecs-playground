1c329a295b423733866887c88ca971736436ee83
MyMilkedEek <eek@mymilkedeek.net>
2017-02-09 23:28:28 +0100

    clear the screen every tick

--- core/src/net/mymilkedeek/ecsplayground/screens/GameScreen.java
+++ core/src/net/mymilkedeek/ecsplayground/screens/GameScreen.java
@@ -2,7 +2,9 @@ package net.mymilkedeek.ecsplayground.screens;
 
 import com.artemis.World;
 import com.artemis.WorldConfigurationBuilder;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import net.mymilkedeek.ecsplayground.GameInstance;
 import net.mymilkedeek.ecsplayground.factories.GameEntityFactory;
@@ -36,6 +38,9 @@ public class GameScreen extends ScreenAdapter {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor( 0, 0, 0, 1 );
+        Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
+
         this.entityWorld.setDelta(delta);
         this.entityWorld.process();
     }
