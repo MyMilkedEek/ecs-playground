9ce2392c6e8ffe0d3663e650f949bffab07c6686
MyMilkedEek <eek@mymilkedeek.net>
2017-02-09 22:48:42 +0100

    added artemis-odb to dependencies

---
+++ core/src/net/mymilkedeek/ecsplayground/components/Position.java
@@ -0,0 +1,11 @@
+package net.mymilkedeek.ecsplayground.components;
+
+import com.artemis.Component;
+
+/**
+ * @author Michaël
+ */
+public class Position extends Component {
+    public float x;
+    public float y;
+}
